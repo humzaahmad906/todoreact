e1a71d134c8245ddb76cdbd72bac561f5074921d
Humza Ahmad <humzaahmad9066@gmail.com>
2020-03-05 19:51:02 +0500

    Todo Added

--- src/App.js
+++ src/App.js
@@ -8,21 +8,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        <h1>A basic Todo List App</h1>
       <TodoList />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
